31076cf314fc9d222c0693c98c8ac943eec83851
Antonio Rivera Alfaro <antonioriveraalfaro@outlook.com>
2026-04-28 00:19:09 +0200

    borrar ejs prep examen

--- src/main/java/ejerciciosTADSExamen/ejercicio3/EmptyGalleryException.java
+++
@@ -1,7 +0,0 @@
-package ejerciciosTADSExamen.ejercicio3;
-
-public class EmptyGalleryException extends Exception {
-    public EmptyGalleryException(String message) {
-        super(message);
-    }
-}
